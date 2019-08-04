cfbb6d36ce0c08813afd48b1f0d2aafa723a1b15
raisa2010 <raisa.kabir2010@gmail.com>
2019-08-04 12:46:10 +0600

    clean up

--- js/main.js
+++ js/main.js
@@ -1,81 +1,55 @@
 AOS.init({once: true});
 
-var parallaxElements = $('#featured-list'),
-  parallaxQuantity = parallaxElements.length
+// Add smooth scrolling to all links in navbar
+$(".nav-item, .nav-link").on('click', function(event) {
+
+  var hash = this.hash;
+  if (hash) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1500, function() {
+      window.location.hash = hash;
+    });
+  }
+});
+
+// Search Animation
+$(".dropdown-item").click(function() {
+  event.preventDefault()
+  $("#dropdownMenuLink").html(this.text)
+})
+
+$(".search-input").focus(function() {
+  $(".expanding-search-form").css("width", "70%")
+}).blur(function() {
+  if ($.trim(this.value).length == 0) {
+    $(".expanding-search-form").css("width", "55%")
+  }
+});
 
   $(window).on('scroll', function() {
     window.requestAnimationFrame(function() {
 
       //Sticky Nav Bar on Scroll
       var navbar = $(".navbar")
-      var nav_links = $(".nav-link")
-      var header_info_container = $(".header-info-container")
-      var scrolled_initial = scrolled;
       var scrolled = $(window).scrollTop()
       var window_height = $(window).height()
       if (scrolled >= 0.03 * window_height) {
         $(".img-responsive").css("transform", "scale(0.8)")
-        // navbar.addClass("fixed-top")
         navbar.addClass("sticky-navbar")
-        // header_info_container.addClass("header-below-nav")
       } else {
-        // navbar.removeClass("fixed-top")
         navbar.removeClass("sticky-navbar")
-        // header_info_container.removeClass("header-below-nav")
         $(".img-responsive").css("transform", "scale(1)")
       }
 
-      $(".featured").css("height",
+      $("#featured").css("height",
       $("#featured-list").outerHeight() + $("#featured-bg-text").outerHeight())
       $(".parallax-container").css("height", $("#featured-list").outerHeight())
 
-      // Search Animation
-      $(".dropdown-item").click(function() {
-        event.preventDefault()
-        $("#dropdownMenuLink").html(this.text)
-      })
-
-      $(".search-input").focus(function() {
-        $(".expanding-search-form").css("width", "70%")
-      }).blur(function() {
-        $(".expanding-search-form").css("width", "55%")
-      });
-
-      // Grid
-      // console.log($(""))
-
-      // Contact Bg Movement
-      // var contact_bg = $('#contact-bg')
-      // if ((scrolled + window_height) >= (contact_bg.offset().top + contact_bg.outerHeight())
-      //   && scrolled + $(".navbar").outerHeight() <= $("#map").offset().top) {
-      //   contact_bg.css({
-      //       'transform' : 'translateX('
-      //       + (contact_bg.offset().top
-      //       + contact_bg.outerHeight()
-      //       - scrolled
-      //       - window_height
-      //       )
-      //       + 'px)'
-      //   });
-      // }
-      //
-      // // $("#whyEduhub").css("top", 0.3 * $("#featured-list").outerHeight())
-
-      // Parallax Scrolling for Featured
-      // var bottom_of_bg = $(".parallax-container").offset().top
-      // var bottom_of_list = $("#featured-list").offset().top
-      // + $("#featured-list").outerHeight()
-      // - 0.78 * $(window).height()
-      // var scrolled = $(window).scrollTop()
-      // var bg_txt = $("#featured-bg-text")
-      //
-      // if (scrolled >= bottom_of_bg && scrolled <= bottom_of_list) {
-      //   bg_txt.addClass("parallax-scroll")
-      // } else {
-      //   bg_txt.attr("data-aos", "fade-up")
-      //   bg_txt.removeClass("parallax-scroll")
-      // }
-
+      if (scrolled + 0.3 * $(window).height() > $(".featured-heading").offset().top) {
+        $(".line").css("height", "180px")
+      }
     })
   })
 
